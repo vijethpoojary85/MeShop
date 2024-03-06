a52a02f5b63d49d669fe11889b9f48f20cb75fc7
vijethpoojary85 <vijethpoojary225@gmail.com>
2024-03-06 22:44:18 +0530

    product listing page added and all the list display done

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { resourceUsage } from 'process';
+import { product } from '../data-type';
 
 @Component({
   selector: 'app-seller-home',
@@ -6,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './seller-home.component.css'
 })
 export class SellerHomeComponent {
+  productList:undefined | product[]
+constructor(private product:ProductService){}
 
+ngOnInit():void{
+  this.product.productList().subscribe((result)=>{
+    console.log(result)
+    this.productList=result;
+  })
+}
 }
